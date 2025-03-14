27926afa250f82c047db10df99b5cc27202a19c7
Gil Rezin <gil.rezin@hotmail.com>
2025-03-14 14:25:50 -0700

    cleaned up code, switched lat and lon into correct positions, added optional debugging for plotting area and way

--- getOSMFiles.py
+++ getOSMFiles.py
@@ -17,12 +17,12 @@ relations = [7820445, 7820446]
 nodesList = []
 boundaryNodes = []
 
-def validate_and_insert_nodes(way, polygon, minx, miny, maxx, maxy):
+def validate_and_insert_nodes(way, polygon):
     
     nodes = way.get_nodes(resolve_missing=True)
 
     # linestring wants longitude before latitude. Not really sure why
-    line = LineString([(node.lon, node.lat) for node in nodes])
+    line = LineString([(node.lat, node.lon) for node in nodes])
 
     # find the intersection of the lines
     intersection = polygon.intersection(line)
@@ -46,22 +46,8 @@ def validate_and_insert_nodes(way, polygon, minx, miny, maxx, maxy):
             boundaryNodes.append(intersection.coords[segment_count])
             segment_count += 1
 
-    # #for node in nodes:
-    # #newNode = Point(node.lat, node.lon)
-    # # check if in proximity to the bbox (speeds up the search process)
-    # #if minx <= newNode.x <= maxx and miny <= newNode.y <= maxy:
-    # # check if the node is beyond the specified bbox
-    # if (polygon.contains(line)):
-    #     nodesList.append(newNode)
-
-    # # check if the node is a boundary node (ex: street connecting to the neighborhood)
-    # elif (polygon.intersects(newNode)):
-    #     boundaryNodes.append(newNode)
-
-    # if neither, discard this node
-
 def get_relation_data(relation):
-    # TODO: the area and rel numbers should come from list relations. Area = rel + 3600000000
+    # call the API with our desired area and relation
     result = api.query("""
     area(""" + str(relation + 3600000000) + """)->.searchArea;
     (
@@ -74,64 +60,54 @@ def get_relation_data(relation):
 
 
     # get our bounding box from our relation
-    bbox = []
     # the items we need to access from our object are:
     # result.relations[0].members[i].geometry[j].lat
     # result.relations[0].members[i].geometry[j].lon
-    for member in result.relations[0].members:
-        if type(member) == overpy.RelationWay:
-            coords = [(geometry.lon, geometry.lat) for geometry in member.geometry]
-            #for geometry in member.geometry:
-                
+    bbox = []
 
-                #lat = geometry.lat
-                #lon = geometry.lon
-                # add the points to our bounding box
-                #bbox.append((lat, lon))
+    for member in result.relations[0].members:
+
+        # only RelationWay objects contain linestrings
+        if type(member) == overpy.RelationWay:
+
+            # create our polygon out of linestrings
+            coords = [(geometry.lat, geometry.lon) for geometry in member.geometry]
             newLineString = LineString(coords)
             bbox.append(newLineString)
 
     # form a polygon from our bounding box
     polygons = list(polygonize(bbox))
-    polygon = polygons[0]
-    #polygon = Polygon(bbox)
-    #polygon = orient(polygon)
-    print(shapely.validation.explain_validity(polygon))
-    minx, miny, maxx, maxy = polygon.bounds
 
-    nodes = result.ways[166].get_nodes(resolve_missing=True)
-    line = LineString([(node.lon, node.lat) for node in nodes])
+    # - DEBUGGING - plot the relation and the way to check for intersection
+    print(shapely.validation.explain_validity(polygons[0]))
+    index = next((i for i, way in enumerate(result.ways) if way.id == 1291524296), -1)
+    nodes = result.ways[index].get_nodes(resolve_missing=True)
+    line = LineString([(node.lat, node.lon) for node in nodes])
     
-    x, y = polygon.exterior.xy  # Extract exterior coordinates
+    x, y = polygons[0].exterior.xy  # Extract exterior coordinates
     # Plot the polygon
-    plt.plot(x, y)
+    plt.plot(y, x)
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     plt.title("Polygon of Geographic Coordinates")
 
     x2,y2 = line.xy
-    plt.plot(x2,y2)
+    plt.plot(y2,x2)
     plt.show()
+    # - END DEBUGGING -
 
-    bound_validate_and_insert_nodes = partial(validate_and_insert_nodes, polygon=polygon, minx=minx, miny=miny, maxx=maxx, maxy=maxy)
+    bound_validate_and_insert_nodes = partial(validate_and_insert_nodes, polygon=polygons[0])
 
     # multithread the node add process (speeds up by many factors)
-    # with ThreadPoolExecutor() as executor:
-    #     executor.map(bound_validate_and_insert_nodes, result.ways)
-    
-    validate_and_insert_nodes(result.ways[132], polygon, minx, miny, maxx, maxy)
+    with ThreadPoolExecutor() as executor:
+        executor.map(bound_validate_and_insert_nodes, result.ways)
             
-
-
     print(nodesList)
+    print(len(nodesList))
     print(boundaryNodes)
-
-
+    print(len(boundaryNodes))
 
 # for every relation, get our neighborhood data
 # for relation in relations:
 #     get_relation_data(relation)
-get_relation_data(3431135)
-
-
-# TODO: save the output to a JSON file
+get_relation_data(9592519)

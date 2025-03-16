0fd7435edc7c2a98287c44a140751575f163a4fc
Gil Rezin <gil.rezin@hotmail.com>
2025-03-15 23:56:54 -0700

    fixed implementation of getting boundary nodesto account for multithreading + bug fixes

--- getOSMFiles.py
+++ getOSMFiles.py
@@ -8,17 +8,24 @@ import matplotlib.pyplot as plt
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 from itertools import groupby
+from queue import Queue
 
 # get our data from the API
 api = overpy.Overpass()
 
-relations = [7820445, 7820446]
+# relations will come from our neighborhoods list
+relations = [7820445, 1384962]
 
-nodesList = []
-boundaryNodes = []
+nodes = Queue()
+boundaryNodes = Queue()
 
+# ensures that the way is valid, then inserts it into the list
 def validate_and_insert_nodes(way, polygon):
     
+    # exclude all non-roads from our dataset
+    if not 'highway' in way.tags:
+        return
+
     nodes = way.get_nodes(resolve_missing=True)
 
     # linestring wants longitude before latitude. Not really sure why
@@ -26,26 +33,44 @@ def validate_and_insert_nodes(way, polygon):
 
     # find the intersection of the lines
     intersection = polygon.intersection(line)
+    boundaryIntersection = (polygon.boundary).intersection(line)
     
     if intersection.is_empty: 
-        # contained entirely within polygon
-        nodesList.append(nodes)
+        # no relation to polygon
+        print("empty line")
+        return
 
     elif isinstance(intersection, LineString):
-        # Single intersection
+        # Single intersection (either partially or entirely within polygon)
         contained_nodes = list(intersection.coords)
-        nodesList.append(contained_nodes)
-        boundaryNodes.append(intersection.coords[0])
+        nodes.put(contained_nodes)
+        nodes.task_done()
+
+        # if partially within the polygon, find the boundary intersection point and add it to the boundaryNodes list
+        if not boundaryIntersection.is_empty:
+            if (type(boundaryIntersection) is shapely.geometry.multipoint.MultiPoint):
+                intersectionPoint = boundaryIntersection.geoms[0]
+            else:
+                intersectionPoint = boundaryIntersection.coords[0]
+            boundaryNodes.put(intersectionPoint)
+            boundaryNodes.task_done()
 
     elif isinstance(intersection, MultiLineString):
         # Multiple intersections
         segment_count = 0
-        for segment in intersection:
+        for segment in intersection.geoms:
             contained_nodes = list(segment.coords)
-            nodesList.append(contained_nodes)
-            boundaryNodes.append(intersection.coords[segment_count])
+            nodes.put(contained_nodes)
+            nodes.task_done()
+
+            # if partially within the polygon, find the boundary intersection point and add it to the boundaryNodes list
+            if not boundaryIntersection.is_empty:
+                intersectionPoint = boundaryIntersection.coords[0]
+                boundaryNodes.put(tuple(intersectionPoint))
+                boundaryNodes.task_done()
             segment_count += 1
 
+# 
 def get_relation_data(relation):
     # call the API with our desired area and relation
     result = api.query("""
@@ -79,21 +104,21 @@ def get_relation_data(relation):
     polygons = list(polygonize(bbox))
 
     # - DEBUGGING - plot the relation and the way to check for intersection
-    print(shapely.validation.explain_validity(polygons[0]))
-    index = next((i for i, way in enumerate(result.ways) if way.id == 1291524296), -1)
-    nodes = result.ways[index].get_nodes(resolve_missing=True)
-    line = LineString([(node.lat, node.lon) for node in nodes])
+    # print(shapely.validation.explain_validity(polygons[0]))
+    # index = next((i for i, way in enumerate(result.ways) if way.id == 97734347), -1)
+    # nodes = result.ways[index].get_nodes(resolve_missing=True)
+    # line = LineString([(node.lat, node.lon) for node in nodes])
     
-    x, y = polygons[0].exterior.xy  # Extract exterior coordinates
-    # Plot the polygon
-    plt.plot(y, x)
-    plt.xlabel("Longitude")
-    plt.ylabel("Latitude")
-    plt.title("Polygon of Geographic Coordinates")
+    # x, y = polygons[0].exterior.xy  # Extract exterior coordinates
+    # # Plot the polygon
+    # plt.plot(y, x)
+    # plt.xlabel("Longitude")
+    # plt.ylabel("Latitude")
+    # plt.title("Polygon of Geographic Coordinates")
 
-    x2,y2 = line.xy
-    plt.plot(y2,x2)
-    plt.show()
+    # x2,y2 = line.xy
+    # plt.plot(y2,x2)
+    # plt.show()
     # - END DEBUGGING -
 
     bound_validate_and_insert_nodes = partial(validate_and_insert_nodes, polygon=polygons[0])
@@ -101,13 +126,34 @@ def get_relation_data(relation):
     # multithread the node add process (speeds up by many factors)
     with ThreadPoolExecutor() as executor:
         executor.map(bound_validate_and_insert_nodes, result.ways)
-            
-    print(nodesList)
-    print(len(nodesList))
-    print(boundaryNodes)
-    print(len(boundaryNodes))
+
+    # way_count = 0
+    # for way in result.ways:
+    #     validate_and_insert_nodes(way, polygons[0]) 
+    #     print(way_count)
+    #     way_count += 1
+
+    # move from queue to list
+    boundaryNodesList = []
+    while not boundaryNodes.empty():
+        boundaryNodesList.append(boundaryNodes.get())
+
+    nodesList = []
+    while not nodes.empty():
+        nodesList.append(nodes.get())
+
+    # - DEBUGGING - display all boundary nodes over the neighborhood
+    # for point in boundaryNodesList:
+    #     plt.plot(y, x)
+    #     plt.plot(point[1], point[0], marker='o', color='orange')
+    # plt.show()
+    # print()
+    # - END DEBUGGING -
 
 # for every relation, get our neighborhood data
-# for relation in relations:
-#     get_relation_data(relation)
-get_relation_data(9592519)
+for relation in relations:
+    print(relation)
+    get_relation_data(relation)
+
+# test for a single relation
+#get_relation_data(1384962)
